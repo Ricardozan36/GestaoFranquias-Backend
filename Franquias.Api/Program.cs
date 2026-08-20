01d756b70d56299587bb717bb0afabfc3b434b1c
Ricardo Zan <ricardozan@outlook.com.br>
2026-08-19 21:52:26 -0300

    feat: instalacao do MudBlazor, chassi do dashboard e tela de login integrada com a API

--- Franquias.Api/Program.cs
+++ Franquias.Api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. INJEÇÃO DE DEPENDÊNCIA DO BANCO DE DADOS (SQLITE)
